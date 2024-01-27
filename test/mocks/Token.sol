ddb2c3bd787172326102fcd6a07e822b4a91478f
polarzero <0xpolarzero@gmail.com>
2024-01-27 11:05:42 +0100

    Use solady for ERC20 token

--- test/mocks/Token.sol
+++ test/mocks/Token.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@solady/tokens/ERC20.sol";
 
+/// @dev An ultra-minimalistic ERC20 token implementation.
 contract Token is ERC20 {
-    constructor(uint256 initialAmount) ERC20("Token", "TKN") {
+    constructor(uint256 initialAmount) {
         _mint(msg.sender, initialAmount);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  METADATA                                  */
+    /* -------------------------------------------------------------------------- */
+
+    function name() public pure override returns (string memory) {
+        return "Token";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "TKN";
     }
 }
