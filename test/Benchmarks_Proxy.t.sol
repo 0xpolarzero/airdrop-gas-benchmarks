257d864fb62ba399df3a34787adb104e53965faf
polarzero <0xpolarzero@gmail.com>
2024-01-30 12:08:00 +0100

    Add proxy deployment measurements

---
+++ test/Benchmarks_Proxy.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {SoladyTest} from "solady/test/utils/SoladyTest.sol";
+import {LibClone} from "@solady/utils/LibClone.sol";
+
+// Calculate the cost of deploying a proxy for Thirdweb contracts
+
+contract ProxyDeployer {
+    function deployProxy(address _implementation) public {
+        LibClone.deployERC1967(_implementation);
+    }
+}
+
+contract Benchmarks_Proxy is SoladyTest {
+    ProxyDeployer deployer;
+
+    function setUp() public {
+        deployer = new ProxyDeployer();
+    }
+
+    function test_deployProxy_Thirdweb(uint256) public {
+        deployer.deployProxy(_randomNonZeroAddress());
+    }
+}
